dba301d64278ba3f3c3d2580b53cf26dae25b732
Oliver109481 <oliver109481@mcpsva.org>
2025-12-04 15:36:45 +0000

    Finished

--- Warm-Up_7_Circle_Calculator.py
+++ Warm-Up_7_Circle_Calculator.py
@@ -1,7 +1,7 @@
 """
 Filename: Warm-Up_7_Circle_Calculator.py
-Author: <Lastname, Firstname>
-Created: <MM/DD/YYYY>
+Author: <Santamaria, Oliver>
+Created: <12/04/2025>
 Instructor: Holtslander
 """
 
@@ -15,8 +15,11 @@ def circle_calculator():
     :return: None
     """
     ### YOUR CODE GOES HERE ###
-    
 
+radius = input("Enter the radius of the circle:\n")
+radius = float(radius)
+print(f"The area of the circle is:\n {3.14*(radius*radius)}")
+print(f"The circumference of the circle is:\n {2*3.14*radius}")
 
 ### YOU SHOULD NOT NEED TO CHANGE ANYTHING HERE ###
 if __name__ == '__main__':
